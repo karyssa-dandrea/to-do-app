9c27151dd5e214cd1dea2cc6f846e2513b8ce53c
karyssa-dandrea <karyssadandrea@gmail.com>
2022-01-11 15:16:55 -0800

    created my form page

--- src/components/Auth/Authform.js
+++ src/components/Auth/Authform.js
@@ -1,5 +1,23 @@
 import React from 'react';
 
-export default function Authform() {
-  return <div></div>;
+export default function Authform({ email, setEmail, password, setPassword, saveButton }) {
+  return (
+    <div className="form">
+      <h1>Sign In or Create An Accont Below:</h1>
+      <div className="user-form">
+        <label>Email:</label>
+        <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
+      </div>
+      <div className="password-form">
+        <label>Password:</label>
+        <input type="text" value={password} onChange={(e) => setPassword(e.target.value)} />
+      </div>
+      <div className="button">
+        <button onClick={saveButton}>Next</button>
+      </div>
+    </div>
+  );
 }
+
+// make an error message later, and a message that it was successful
+// create a loading state
