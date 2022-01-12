5fd9e4510b8a7765628a5947f487a5dee7aa777a
karyssa-dandrea <karyssadandrea@gmail.com>
2022-01-11 17:01:53 -0800

    added my functions for todos

---
+++ src/views/Todos/todos.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function todos() {
+  return <div></div>;
+}
+
+// const task, settask =usestate('');
+
+// task state
+// list of tasks
+// useeffect that grabs all tasks associated with that user
